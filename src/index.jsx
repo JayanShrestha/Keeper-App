1361a018e97b5973fb6ac99d5fffc777860a97d9
Jayan Shrestha <jayanshr44@gmail.com>
2026-02-13 22:31:41 +1100

    Update react and react-dom

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
 
+createRoot(document.getElementById("root")).render(
+
+    <StrictMode>
+      <App />
+    </StrictMode>
+ 
+);
